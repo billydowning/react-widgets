82b8c84b748b99559c8da49e4c624c2ca594a8e7
Billy Downing <wldowning@gmail.com>
2020-10-26 14:25:48 -0600

    working translate component

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+
   useEffect(() => {
-    console.log('New language or text');
+    const doTranslation = async () => {
+       const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text,
+          target: language.value,
+          key: '',
+        },
+      }
+    );
+
+    setTranslated(data.data.translations[0].translatedText);
+  };
+
+    doTranslation();
   }, [language, text]);
 
-  return <div>Convert</div>
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+    );
 };
 
 
