c1248a968501079254370e61d3313a8c47676870
Billy Downing <wldowning@gmail.com>
2020-10-25 18:34:47 -0600

    translate widget

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+
+const options = [
+
+  {
+    label: 'Afrikaans',
+    value: 'af'
+  },
+  {
+    label: 'Arabic',
+    value: 'ar',
+  },
+  {
+    label: 'Hindi',
+    value: 'hi'
+  },
+];
+
+
 
 const Translate = () => {
-  return <div>Translate</div>;
-}
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
+
+  return (
+    <div className="ui container">
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="">Enter Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+      <Dropdown
+        label="Select A Language"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+    </div>
+    );
+};
 
 export default Translate;
